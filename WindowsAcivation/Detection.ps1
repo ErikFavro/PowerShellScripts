9e362833d7b9a3cb41aa327d1b556765dcf87578
Ellipsia <80127443+Ellipsia@users.noreply.github.com>
2024-09-27 15:30:26 -0700

    Initial commit

---
+++ WindowsAcivation/Detection.ps1
@@ -0,0 +1,12 @@
+﻿$LicensedStatusofOS = Get-CimInstance SoftwareLicensingProduct -Filter "Name like 'Windows%'" | where { $_.PartialProductKey } | select LicenseStatus
+$OSProductDescription = Get-CimInstance SoftwareLicensingService | Select OA3xOriginalProductKeyDescription
+
+
+If (($LicensedStatusofOS -ne '1') -and (($OSProductDescription -eq '[4.0] Core OEM:DM') -or ($OSProductDescription -eq $null))){
+    
+    Exit 1
+}
+else
+{
+    Exit 0
+}
---
+++ WindowsAcivation/Remediation.ps1
@@ -0,0 +1,2 @@
+﻿cscript.exe slmgr.vbs /ipk <Add Product Key Here>
+Cscript.exe slmgr.vss /ato
