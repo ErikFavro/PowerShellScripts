8ffd94cecbe845c1057d464ecda809422d4650f6
Ellipsia <80127443+Ellipsia@users.noreply.github.com>
2025-04-15 14:32:01 -0700

    Added logic for local users

--- DeadProfileList/RemoveDeadProfilesList.ps1
+++ DeadProfileList/RemoveDeadProfilesList.ps1
@@ -1,13 +1,20 @@
-﻿#omit not domained users, anything with default in it?
-$ExcludeUsers = ("Public", "Default", "All Users", "Default User", "Administrator")
-$UserList = @(Get-ChildItem "\\ATH-8t5J6y1\C$\Users" | Select Name)
+﻿$ComputerToAudit = Read-Host -Prompt "Please enter a computer name"
+
+#omit not domained users, anything with default in it?
+$ExcludeUsers = @("Public", "Default", "All Users", "Default User", "Administrator", "Default.migrated", "DefaultAccount", "Guest", "ADMINI~1")
+$UserList = @(Get-ChildItem "\\$ComputerToAudit\C$\Users" | Select Name)
+$localUserList = @(Invoke-Command -ComputerName "$ComputerToAudit" -ScriptBlock {Get-LocalUser} | Select Name)
 
 ForEach ($CheckExclude in $ExcludeUsers){
     foreach ($CurrentUser in $UserList.Name){
-        If ($checkExclude -eq $CurrentUser){
+        If ($checkExclude -like $CurrentUser){
             $UserList = $UserList -notmatch $CurrentUser
         }
     }
+}
+
+ForEach ($CurrentUserAudit in $localUserList){
+    $UserList = $UserList -notmatch $CurrentUserAudit
 }
 
 ForEach ($User in $UserList){
