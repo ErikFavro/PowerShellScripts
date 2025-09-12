36a0794e86c5f33e0da55ef7b1618f75b8f43639
Erik Favro <80127443+ErikFavro@users.noreply.github.com>
2025-09-12 16:12:32 -0700

    Added scripts

---
+++ MassUpdateCollectionQuery/Push-MassUpdateCollectionQuery.ps1
@@ -0,0 +1,19 @@
+﻿$GatheredCollections = Import-Csv -Path "C:\Temp\CollectionGather.csv"
+$OutputFile = "C:\Temp\CollectionWithQuery.csv"
+$Results = @()
+$ObjectResults = @()
+
+foreach ($ProcessColelction in $GatheredCollections){
+
+    $ObjectResults = [PSCustomObject]@{
+        "CollectionName"  = $ProcessColelction.CollectionName
+        "CollectionID"    = $ProcessColelction.CollectionID
+        "QueryExpression" = ""
+    }
+
+    $ObjectResults.QueryExpression = (Get-CMDeviceCollectionQueryMembershipRule -CollectionId $ProcessColelction.CollectionID).QueryExpression
+
+    $Results += $ObjectResults
+}
+
+$Results | Export-Csv -Path $OutputFile -NoTypeInformation
