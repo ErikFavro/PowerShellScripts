2464405ee96c0578e879c3c8c0e2445328ad9e53
Erik Favro <80127443+ErikFavro@users.noreply.github.com>
2025-09-15 11:09:44 -0700

    Removed verbose

--- MassUpdateCollectionQuery/Push-MassUpdateCollectionQuery.ps1
+++ MassUpdateCollectionQuery/Push-MassUpdateCollectionQuery.ps1
@@ -5,7 +5,8 @@ $ObjectResults = @()
 
 foreach ($ProcessCollection in $UpdateCollections){
 
-    Remove-CMDeviceCollectionQueryMembershipRule -CollectionId $ProcessCollection.CollectionID -RuleName $ProcessCollection.RuleName -Force -Confirm:$false -Verbose
+    Remove-CMDeviceCollectionQueryMembershipRule -CollectionId $ProcessCollection.CollectionID -RuleName $ProcessCollection.RuleName -Force -Confirm:$false
     Add-CMDeviceCollectionQueryMembershipRule -CollectionId $ProcessCollection.CollectionID -RuleName $ProcessCollection.RuleName -QueryExpression $ProcessCollection.QueryExpression
 
 }
+
