4ecd16164ef2b0b0588224bbe4bc8f7aa36d6c60
Erik Favro <80127443+ErikFavro@users.noreply.github.com>
2026-03-05 16:11:44 -0800

    New script

---
+++ EntraGroupBulkCreation/DellModelBulkCreation.ps1
@@ -0,0 +1,17 @@
+﻿Install-Module AzureADPreview -AllowClobber -Confirm:$false
+Import-Module AzureADPreview
+Connect-AzureAD
+
+$Manufacturer = "Dell"
+$ModelGroup = "device.dell."
+$GroupType = ".dynamic"
+$DescriptionType = "dynamic group"
+
+$DellModelList = Import-Csv "C:\Temp\DellModelFullList.csv"
+
+foreach ($CurrentModel in $DellModelList){ 
+    $DisplayName = $ModelGroup + ($CurrentModel).DisplayName + $GroupType
+    $MembershipRule = '(device.deviceModel -eq ' + '"' + ($CurrentModel).MembershipRule + "`")"
+    $Description = $Manufacturer + ' ' + $CurrentModel.Description + ' ' + $DescriptionType
+    New-AzureADMSGroup -DisplayName $DisplayName -Description $Description -MailEnabled $false -SecurityEnabled $true -MailNickname $CurrentModel.MailNickname -GroupTypes "DynamicMembership" -MembershipRule $MembershipRule -MembershipRuleProcessingState "On" -Verbose
+}
