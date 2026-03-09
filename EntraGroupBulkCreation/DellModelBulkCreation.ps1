8cbf646ce8b28dd645248fd85ff1b1ec12bf2618
Erik Favro <80127443+ErikFavro@users.noreply.github.com>
2026-03-09 08:23:04 -0700

    Added notes

--- EntraGroupBulkCreation/DellModelBulkCreation.ps1
+++ EntraGroupBulkCreation/DellModelBulkCreation.ps1
@@ -1,4 +1,10 @@
-﻿Install-Module AzureADPreview -AllowClobber -Confirm:$false
+﻿#Create 4 columns in CSV DisplayName	Description	MailNickname	MemberShipRule
+#DisplayName: Model of computer in lowercase and no spaces: latitude3420
+#Description: Used in group naming, keep capital with spaces: Latitude 3420
+#MailNickname: Same as display name, lowercase with no spaces: latitude3420
+#MemberShipRule: Same as description: Latitude 3420
+
+Install-Module AzureADPreview -AllowClobber -Confirm:$false
 Import-Module AzureADPreview
 Connect-AzureAD
 
